5f6af586f705ff63de5a34dc4eafdf68b2359cda
Mpiffer <121992959+Mpiffer@users.noreply.github.com>
2023-05-27 20:49:16 -0300

    Update indextest.js

--- indextest.js
+++ indextest.js
@@ -1,5 +1,5 @@
 const wa = require('@open-wa/wa-automate');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 wa.create({
   sessionId: "COVID_HELPER",
@@ -14,54 +14,31 @@ wa.create({
   qrTimeout: 0,
 }).then(client => start(client));
 
-async function queryFlowise(data) {
-  try {
-    const response = await fetch(
-      "http://localhost:3000/api/v1/prediction/2f3522c3-1e9f-4f2e-a411-f34303e98cd2",
-      {
-        headers: { Authorization: "Bearer kXdC9cC0Cm4iHbmzAtxVdZc+YkhFFH3Afd56xFj6lok=" },
-        method: "POST",
-        body: JSON.stringify(data)
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error(`Erro ao consultar o Flowise: ${response.status} ${response.statusText}`);
-    }
-
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.error("Erro ao fazer a consulta ao Flowise:", error);
-    throw error;
-  }
-}
-
-async function start(client) {
+function start(client) {
   client.onMessage(async message => {
-    try {
-      const userMessage = message.body.toLowerCase();
+    if (message.body === 'Hi') {
+      await client.sendText(message.from, '👋 Olá!');
+    } else {
+      // Enviar a pergunta para o endpoint da API
+      const question = message.body;
+      const token = 'pOZiOWTf4aDiBD2PinQyX9nEjXstIPeGecqUx2onR/E='; // Substitua pelo seu token de autorização
 
-      if (userMessage === 'oi') {
-        await client.sendText(message.from, '👋 Olá!');
-      } else {
-        const queryData = { question: userMessage };
-        const flowiseResponse = await queryFlowise(queryData);
+      try {
+        const response = await axios.post('http://192.168.15.8:3000/api/v1/prediction/2f3522c3-1e9f-4f2e-a411-f34303e98cd2', {
+          question: question
+        }, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
 
-        const answer = flowiseResponse?.answer || 'Desculpe, não tenho uma resposta para isso no momento.';
+        const answer = response.data; // Obter a resposta da API
+
         await client.sendText(message.from, answer);
+      } catch (error) {
+        console.error(error);
+        await client.sendText(message.from, 'Desculpe, ocorreu um erro ao processar sua pergunta.');
       }
-    } catch (error) {
-      console.error("Erro ao processar a mensagem:", error);
     }
   });
 }
-
-// Exemplo de uso da função query
-queryFlowise({ question: "Hey, how are you?" })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.error("Erro ao fazer a consulta ao Flowise:", error);
-  });
